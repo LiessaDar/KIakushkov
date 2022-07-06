f57f246e26cab7388b3caf29dd57c047297bdafe
LiessaDar <anj9612@mail.ru>
2022-07-06 13:29:58 +0300

    version 1.0 - Task 1 completed, Home page "Hello world" released.

--- telegram-bot/src/main/java/ru/LiessaDar/telegrambot/TelegramBotApplication.java
+++ telegram-bot/src/main/java/ru/LiessaDar/telegrambot/TelegramBotApplication.java
@@ -2,8 +2,10 @@ package ru.LiessaDar.telegrambot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//@ComponentScan
 public class TelegramBotApplication {
 
 	public static void main(String[] args) {
